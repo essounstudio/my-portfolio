28a895678c172f024d762f36f21649779b105de0
essounstudio <essounstudio@gmail.com>
2025-10-17 16:30:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+navbarToggle.addEventListener("click", () => {
+  navbarToggle.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
+});
+
+const animatedElements = document.querySelectorAll(
+  ".fade-in, .slide-in-left, .slide-in-right, .zoom-in"
+);
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
